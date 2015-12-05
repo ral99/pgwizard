d6ec63d31554a092a8c8c48e98bda215b06ed4de
Rodrigo A. Lima <rodrigoalima99@gmail.com>
2015-12-05 17:16:32 -0200

    Test connections after an interval.

--- src/pgwizard/connection.py
+++ src/pgwizard/connection.py
@@ -1,5 +1,6 @@
 import psycopg2
 import random
+import datetime
 
 class PGWizardCursor:
     """Cursor for a PostgreSQL database connection."""
@@ -65,8 +66,9 @@ class PGWizardTransactionalConnection(PGWizardConnection):
 class PGWizardConnectionPool:
     """Pool of connections to PostgreSQL databases."""
 
-    def __init__(self):
-        """Initialize the containers for connections."""
+    def __init__(self, connection_test_interval_in_seconds):
+        """Initialize the containers for connections and set attributes."""
+        self._connection_test_interval = datetime.timedelta(seconds=connection_test_interval_in_seconds)
         self._master = {}
         self._slave = {}
 
@@ -93,7 +95,9 @@ class PGWizardConnectionPool:
                 'user': user,
                 'password': password,
                 'connection': None,
-                'transactional_connection': None
+                'transactional_connection': None,
+                'connection_test_expiration_time': None,
+                'transactional_connection_test_expiration_time': None
         }
 
     def add_slave_database_server(self, name, database, host, port, user, password):
@@ -107,69 +111,87 @@ class PGWizardConnectionPool:
             'user': user,
             'password': password,
             'connection': None,
-            'transactional_connection': None
+            'transactional_connection': None,
+            'connection_test_expiration_time': None,
+            'transactional_connection_test_expiration_time': None
         })
 
     def get_master_connection(self, name):
         """Return the master database server connection."""
         master = self._master[name]
-        try:
-            connection = master['connection']
-            cursor = connection.cursor()
-            cursor.execute("SELECT 1")
-            cursor.close()
-        except:
-            connection = psycopg2.connect(database=master['database'], host=master['host'], port=master['port'],
-                                          user=master['user'], password=master['password'])
-            connection.autocommit = True
-            master['connection'] = connection
+        now = datetime.datetime.now()
+        expiration_time = master['connection_test_expiration_time']
+        if expiration_time is None or expiration_time < now:
+            master['connection_test_expiration_time'] = now + self._connection_test_interval
+            try:
+                connection = master['connection']
+                cursor = connection.cursor()
+                cursor.execute("SELECT 1")
+                cursor.close()
+            except:
+                connection = psycopg2.connect(database=master['database'], host=master['host'], port=master['port'],
+                                              user=master['user'], password=master['password'])
+                connection.autocommit = True
+                master['connection'] = connection
         return PGWizardConnection(master['connection'])
 
     def get_slave_connection(self, name):
         """Return a random slave database server connection."""
         slave = random.choice(self._slave[name])
-        try:
-            connection = slave['connection']
-            cursor = connection.cursor()
-            cursor.execute("SELECT 1")
-            cursor.close()
-        except:
-            connection = psycopg2.connect(database=slave['database'], host=slave['host'], port=slave['port'],
-                                          user=slave['user'], password=slave['password'])
-            connection.autocommit = True
-            slave['connection'] = connection
+        now = datetime.datetime.now()
+        expiration_time = slave['connection_test_expiration_time']
+        if expiration_time is None or expiration_time < now:
+            slave['connection_test_expiration_time'] = now + self._connection_test_interval
+            try:
+                connection = slave['connection']
+                cursor = connection.cursor()
+                cursor.execute("SELECT 1")
+                cursor.close()
+            except:
+                connection = psycopg2.connect(database=slave['database'], host=slave['host'], port=slave['port'],
+                                              user=slave['user'], password=slave['password'])
+                connection.autocommit = True
+                slave['connection'] = connection
         return PGWizardConnection(slave['connection'])
 
     def get_master_transactional_connection(self, name):
         """Return the master database server transactional connection."""
         master = self._master[name]
-        try:
-            transactional_connection = master['transactional_connection']
-            cursor = transactional_connection.cursor()
-            cursor.execute("SELECT 1")
-            transactional_connection.commit()
-            cursor.close()
-        except:
-            transactional_connection = psycopg2.connect(database=master['database'], host=master['host'],
-                                                        port=master['port'], user=master['user'],
-                                                        password=master['password'])
-            transactional_connection.autocommit = False
-            master['transactional_connection'] = transactional_connection
+        now = datetime.datetime.now()
+        expiration_time = master['transactional_connection_test_expiration_time']
+        if expiration_time is None or expiration_time < now:
+            master['transactional_connection_test_expiration_time'] = now + self._connection_test_interval
+            try:
+                transactional_connection = master['transactional_connection']
+                cursor = transactional_connection.cursor()
+                cursor.execute("SELECT 1")
+                transactional_connection.commit()
+                cursor.close()
+            except:
+                transactional_connection = psycopg2.connect(database=master['database'], host=master['host'],
+                                                            port=master['port'], user=master['user'],
+                                                            password=master['password'])
+                transactional_connection.autocommit = False
+                master['transactional_connection'] = transactional_connection
         return PGWizardTransactionalConnection(master['transactional_connection'])
 
     def get_slave_transactional_connection(self, name):
         """Return a random slave database server transactional connection."""
         slave = random.choice(self._slave[name])
-        try:
-            transactional_connection = slave['transactional_connection']
-            cursor = transactional_connection.cursor()
-            cursor.execute("SELECT 1")
-            transactional_connection.commit()
-            cursor.close()
-        except:
-            transactional_connection = psycopg2.connect(database=slave['database'], host=slave['host'],
-                                                        port=slave['port'], user=slave['user'],
-                                                        password=slave['password'])
-            transactional_connection.autocommit = False
-            slave['transactional_connection'] = transactional_connection
+        now = datetime.datetime.now()
+        expiration_time = slave['transactional_connection_test_expiration_time']
+        if expiration_time is None or expiration_time < now:
+            slave['transactional_connection_test_expiration_time'] = now + self._connection_test_interval
+            try:
+                transactional_connection = slave['transactional_connection']
+                cursor = transactional_connection.cursor()
+                cursor.execute("SELECT 1")
+                transactional_connection.commit()
+                cursor.close()
+            except:
+                transactional_connection = psycopg2.connect(database=slave['database'], host=slave['host'],
+                                                            port=slave['port'], user=slave['user'],
+                                                            password=slave['password'])
+                transactional_connection.autocommit = False
+                slave['transactional_connection'] = transactional_connection
         return PGWizardTransactionalConnection(slave['transactional_connection'])
